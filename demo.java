11159056a956bd7e0598a973f276782f450917a1
Rahulkaitha <126646931+Rahulkaitha@users.noreply.github.com>
2023-03-01 14:27:46 +0530

    Update demo.java

--- demo.java
+++ demo.java
@@ -6,7 +6,6 @@ public class demo {
 		System.out.println("hello guys this is demo code");
 		// this is added in web
 		System.out.println("this is included in version 1");
-		System.out.println("this is modified by rk");
 
 	}
 
