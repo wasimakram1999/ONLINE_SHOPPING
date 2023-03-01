19a65fe5b1c3a747f6494ffe93569ffc890fb307
wasimakram1999 <wasim.akram@indiumsoft.com>
2023-03-01 13:42:30 +0530

    create demo

---
+++ demo.java
@@ -0,0 +1,11 @@
+package demo;
+
+public class demo {
+
+	public static void main(String[] args) {
+		System.out.println("hello guys this is demo code");
+		// TODO Auto-generated method stub
+
+	}
+
+}
